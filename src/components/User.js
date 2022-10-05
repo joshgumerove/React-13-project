18b6c682b971d8daf3d85f11b782329e07029046
Josh Gumerove <jgumerove@kaufmanrossin.com>
2022-10-05 12:46:53 -0400

    add all files

---
+++ src/components/User.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import classes from "./User.module.css";
+
+class User extends Component {
+  render() {
+    const { name } = this.props;
+    return <li className={classes.user}>{name}</li>;
+  }
+}
+
+// const User = (props) => {
+//   return <li className={classes.user}>{props.name}</li>;
+// };
+
+export default User;
